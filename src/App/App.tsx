e8c19a634773258777d1411fab05c05b7a42daab
Julio Romero <julio_ale21@hotmail.com>
2021-04-08 18:01:44 -0300

    feat(items): added user can see item list and remove them

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,48 @@
 import * as React from "react";
 
+import api from "../item/api";
+import {Item} from "../item/types";
 import Button from "../ui/controls/buttons";
 
 import styles from "./App.module.scss";
 
+enum Status {
+  Init = "init",
+  Success = "success",
+}
+
 const App: React.FC = () => {
+  const [items, setItems] = React.useState<Item[]>([]);
+  const [status, setStatus] = React.useState<Status>(Status.Init);
+
+  React.useEffect(() => {
+    api.list().then((items) => {
+      setItems(items);
+      setStatus(Status.Success);
+    });
+  }, []);
+
+  function handleDeleteItem(id: number) {
+    api.remove(id).then(() => setItems((items) => items.filter((item) => item.id !== id)));
+  }
+
+  if (status === Status.Init) return <span>Loading...</span>;
+
   return (
     <main className={styles.container}>
       <header className={styles.header}>
-        <h1>
-          <img alt="TradeHelm" src="/logo.svg" />
-        </h1>
-        <h3>Lets get this party started</h3>
+        <h1>Supermarket List</h1>
+        <h3>{items.length} items</h3>
       </header>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <span>{item.text}</span>
+            <button onClick={() => handleDeleteItem(item.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
 
       <Button colorScheme="primary">Add Item</Button>
     </main>
