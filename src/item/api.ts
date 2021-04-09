2fa3b161bdef9d3a276e82ce8f1ad079aca44e79
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-04-09 12:27:58 -0300

    chore: added local storage and set time out to simulate a api call

--- src/item/api.ts
+++ src/item/api.ts
@@ -1,13 +1,57 @@
 import { Item } from "./types";
-
+/* 
 const MOCK: Item[] = [
   { id: 1, text: "helado de pejelagarto" },
   { id: 2, text: "sandwich de pejelagarto" },
   { id: 3, text: "Sushi de pejelagarto" },
-];
+]; */
+
+function getFromLocalStorage(key: string) {
+  let items: Item[] = [];
+  const result = localStorage.getItem(key) || "";
+
+  if (result !== "") {
+    items = JSON.parse(result);
+  }
+
+  return items;
+}
 
 export default {
-  list: (): Promise<Item[]> => Promise.resolve(MOCK),
-  create: (text: Item["text"]): Promise<Item> => Promise.resolve({ id: +new Date(), text }),
-  remove: (id: Item["id"]): Promise<Item["id"]> => Promise.resolve(id),
+  list: (): Promise<Item[]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const items: Item[] = getFromLocalStorage("items");
+
+        return resolve(items);
+      }, 2000);
+    });
+  },
+
+  create: (text: Item["text"]): Promise<Item> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const item: Item = { id: +new Date(), text };
+        const items: Item[] = getFromLocalStorage("items");
+
+        items.push(item);
+
+        localStorage.setItem("items", JSON.stringify(items));
+
+        return resolve(item);
+      }, 1000);
+    });
+  },
+
+  remove: (id: Item["id"]): Promise<Item["id"]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const items: Item[] = getFromLocalStorage("items");
+
+        localStorage.setItem("items", JSON.stringify(items.filter((item) => item.id !== id)));
+
+        return resolve(id);
+      }, 1000);
+    });
+  },
 };
