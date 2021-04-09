e2843bc3cc6c15db495f09c53871ed52be8727d4
Julio Romero <julio_ale21@hotmail.com>
2021-04-09 00:17:46 -0300

    chore: added List, ListItem and styles

---
+++ src/item/components/List.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./List.module.scss";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
+
+const List: React.FC<Props> = () => {
+  return <div>{`<List />`}</div>;
+};
+
+export default List;
